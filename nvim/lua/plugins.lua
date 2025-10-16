e9a1e298f4c6dbc682d634b02dc29065876da0d7
Bexultan Rakhim <bexultanrakhim@Bexultans-MacBook-Air.local>
2025-10-16 14:18:10 +0900

    rm: disable fzf

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -46,33 +46,32 @@ return require("lazy").setup({
     -- }}}
 
     -- {{{ Search
-    {
-        "https://github.com/ibhagwan/fzf-lua",
-        dependencies = {
-            "https://github.com/elanmed/fzf-lua-frecency.nvim",
-        },
-        event = "VeryLazy",
-        config = function()
-            require("fzf-lua").setup({
-                "max-perf",
-                winopts = {
-                    height = 0.5,
-                    width = 1,
-                    row = 1,
-                },
-            })
-            require("fzf-lua").register_ui_select()
-            require("fzf-lua-frecency").setup({
-                display_score = false,
-            })
-        end,
-    },
+    -- {
+    --     "https://github.com/ibhagwan/fzf-lua",
+    --     dependencies = {
+    --         "https://github.com/elanmed/fzf-lua-frecency.nvim",
+    --     },
+    --     event = "VeryLazy",
+    --     config = function()
+    --         require("fzf-lua").setup({
+    --             "max-perf",
+    --             winopts = {
+    --                 height = 0.5,
+    --                 width = 1,
+    --                 row = 1,
+    --             },
+    --         })
+    --         require("fzf-lua").register_ui_select()
+    --         require("fzf-lua-frecency").setup({
+    --             display_score = false,
+    --         })
+    --     end,
+    -- },
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
         dependencies = { {'nvim-lua/plenary.nvim'} },
         config = function()
-        -- 5.5. Telescope (File Finder) Configuration
         require('telescope').setup({
             defaults = {
                 mappings = {
