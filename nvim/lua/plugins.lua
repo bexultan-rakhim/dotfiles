89e0c4bd4463027c3e92592314380a950990ae3d
Bexultan Rakhim <bexultan.rakhim@gmail.com>
2025-11-07 00:37:13 +0900

    fix: no more neo tree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -204,38 +204,10 @@ return require("lazy").setup({
         end,
     },
     {
-    "nvim-neo-tree/neo-tree.nvim",
-    cmd = "Neotree", -- Use cmd for lazy loading
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
-    config = function()
-      -- 5.3. Neo-tree (File Explorer) Configuration
-      require("neo-tree").setup({
-        window = {
-          position = "left",
-          width = 35,
-          mapping_options = {
-            noremap = true,
-            nowait = true,
-          },
-        },
-        filesystem = {
-          filtered_items = {
-            hide_dotfiles = false,
-            hide_hidden = false,
-          }
-        },
-        default_component_configs = {
-          git_status = {
-            symbols = {
-              added = "✚",
-              modified = "",
-              deleted = "✖",
-              untracked = "⋆",
-            },
-          },
-        }
-      })
-    end,
-  },
+        "nvim-treesitter/nvim-treesitter",
+        branch = 'master',
+        lazy = false,
+        build = ":TSUpdate",
+    },
     -- }}}
 })
