49bea2f5d060fccd814efe154106fd673775eb06
Bexultan Rakhim <bexultan.rakhim@gmail.com>
2026-02-11 23:55:01 +0900

    fix: display manager

--- nixos/common/configuration.nix
+++ nixos/common/configuration.nix
@@ -1,22 +1,11 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -26,7 +15,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -39,49 +27,52 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.xserver.displayManager.setupCommands = ''
-    ${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --primary --auto --output HDMI-0 --left-of DP-0 --auto
-  '';  
-  services.xserver.windowManager.i3 = {
+  services.displayManager.ly.enable = true;
+  # X11 settings.
+  services.xserver = {
     enable = true;
-    extraPackages = with pkgs; [
-        dmenu # common for i3
-        i3status
-        i3lock
-    ];
+    # Configure keymap
+    xkb = {
+      layout = "us,kz";
+      variant = ",";
+      options = "grp:win_space_toggle";
+    };
+   displayManager.setupCommands = ''
+${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --primary --auto --output HDMI-0 --left-of DP-0 --auto
+    ''; 
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+          dmenu # common for i3
+          i3status
+          i3lock
+      ];
+    };
   };
 
   services.picom = {
    enable = true;
    fade = true;
    inactiveOpacity = 0.9;
-  settings = {
-    corner-radius = 10;
-    blur-method = "dual_kawase";
-    blur-strength = 5;
-  };
+   settings = {
+     corner-radius = 20;
+     blur-method = "dual_kawase";
+     blur-strength = 5;
+   };
    shadow = true;
-   backend = "glx"; # Better performance for most hardware
+   backend = "glx";
   }; 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us,kz";
-    variant = ",";
-    options = "grp:win_space_toggle";
+
+  # Services
+  services.printing.enable = true;
+  services.openssh.enable = true;
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # Sound
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -96,11 +87,17 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-  services.ollama = {
-    enable = true;
-    acceleration = "cuda";
+
+  users.users.bex = {
+    isNormalUser = true;
+    description = "bex";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
   };
 
+  # Fonts & Programs
   fonts.packages = with pkgs; [
     inter
     noto-fonts-cjk-sans
@@ -109,31 +106,9 @@
     nerd-fonts.iosevka
     nerd-fonts.hasklug
   ];
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.bex = {
-    isNormalUser = true;
-    description = "bex";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
-  };
-
-  # Install firefox.
-  programs = {
-    firefox.enable = true;
-  };
-
-  # Allow unfree packages
+  programs.firefox.enable = true;
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     feh
     alacritty
@@ -163,23 +138,8 @@
     i3status-rust
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  services.openssh.enable = true;
-
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 3000 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
